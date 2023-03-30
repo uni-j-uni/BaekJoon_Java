aaf3dedb145c8a01fb6877782608b36e2c36742c
HeeJun <unijun0109@gmail.com>
2023-03-30 22:21:41 +0900

    March 30th

---
+++ BaekJoon_Level_1/src/Level1_12.java
@@ -0,0 +1,10 @@
+// 고양이
+public class Level1_12 {
+	public static void main(String args[])
+	{
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.print(" \\(__)|");
+	}
+}
